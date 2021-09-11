71c835c4b79b9034561ece6578c4b8fee3fb683f
elMuso <42069617+elMuso@users.noreply.github.com>
2021-09-11 16:38:07 -0400

    Update entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh -l
 
-cd "resources"
-echo "$PATH"
-ls
-qpdf --empty --pages *.pdf -- combined.pdf
-echo "::set-output result=$INPUTFOLDER/combined.pdf"
+cd "$inputpath"
+
+qpdf --empty --pages *.pdf -- $outputName.pdf
+echo "::set-output result=$inputpath/$outputName.pdf"
